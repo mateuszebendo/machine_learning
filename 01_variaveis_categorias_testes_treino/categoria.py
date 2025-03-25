17109e9590f8fe8686cf044979f5fb924780112a
mateuszebendo <mateuszebendovp30@gmail.com>
2025-03-24 21:35:19 -0300

    feat: adicona N aulas

---
+++ 01_variaveis_categorias_testes_treino/categoria.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import pandas as pd
+ 
+baseDeDados = pd.read_csv('admission.csv', delimiter=';')
+X = baseDeDados.iloc[:, :-1].values
+
+#recebe as variavies dependentes (admissao baseada nas notas)
+Y = baseDeDados.iloc[:, -1].values
+
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+imputer = imputer.fit_transform(X[:, 1:])
+
+from sklearn.preprocessing import LabelEncoder
+
+#transforma campos nao numericos em variaveis categoricas (rotulos)
+label_enconder_X = LabelEncoder() 
+X[:, 0] = label_enconder_X.fit_transform(X[:, 0])
+
+X = X[:, 1:]
+#one hot enconding - transforma variaveis categoricas em valores binarios
+D = pd.get_dummies(X[:, 0])
+X = np.insert(X, 0, D.values, axis=1)
+
+from sklearn.model_selection import train_test_split
+#separacao dos dados em 20% para testes e 80% para treinos
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+print(X_test)
---
+++ 02_normalizando_conjunto_de_dados/scaling.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import pandas as pd 
+
+print("Carrengo a base de dados...")
+baseDeDados = pd.read_csv("admission.csv", delimiter=";")
+X = baseDeDados.iloc[:, :-1].values
+Y = baseDeDados.iloc[:, -1].values 
+
+print("Preenchendo os dados que estao faltando...")
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy="median")
+imputer = imputer.fit_transform(X[:, 1:])
+print("ok!")
+
+print("Computando rotulação...")
+from sklearn.preprocessing import LabelEncoder
+label_encoder_X = LabelEncoder()
+X[:, 0] = label_encoder_X.fit_transform(X[:, 0])
+
+X = X[:, 1:]
+D = pd.get_dummies(X[:, 0])
+X = np.insert(X, 0, D.values, axis=1)
+print("ok!")
+
+print("Separando conjuntos de teste e treino...")
+from sklearn.model_selection import train_test_split
+XTrain, XTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2)
+print("ok!")
+
+print("Computando normalização...")
+from sklearn.preprocessing import StandardScaler
+scaleX = StandardScaler()
+XTrain = scaleX.fit_transform(XTrain)
+XTest = scaleX.fit_transform(XTest)
+print("ok!")
